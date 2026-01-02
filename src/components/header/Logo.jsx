d64c4b58d53575a8c058e448cadee0a3383baa87
tomej-dev <joaocarlos8221@gmail.com>
2026-01-02 17:31:26 -0300

    fix logo

--- src/components/header/Logo.jsx
+++ src/components/header/Logo.jsx
@@ -1,6 +1,6 @@
 export default function Logo(){
     return(
-        <div className="text-5xl font-bold">
+        <div className="text-5xl font-bold hidden sm:block">
             <h1>JT</h1>
         </div>
     )
