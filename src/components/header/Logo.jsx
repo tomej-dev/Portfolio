489505da4b6ae7d9524f76dfe320f15f87aca209
tomej-dev <joaocarlos8221@gmail.com>
2026-01-02 16:09:26 -0300

    fix

--- src/components/header/Logo.jsx
+++ src/components/header/Logo.jsx
@@ -1,7 +1,7 @@
 export default function Logo(){
     return(
         <div className="text-6xl font-bold">
-            <a href="inicio"><h1>JT</h1></a>
+            <h1>JT</h1>
         </div>
     )
 }
